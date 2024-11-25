fd6f542ee5e7d4de16f3ce1da63f99c50af581c6
EsmeMejia29 <esmejia2908@gmail.com>
2024-11-25 13:10:55 -0600

    Add files via upload
    Información de la comida para poner dentro de la app

---
+++ diccionariosStock.py
@@ -0,0 +1,55 @@
+#### Información para poner dentro de la app ####
+
+Papas = {
+    "Papas Francesas": [1.99, "Papas fritas", "papasFrancesas.jpg", "Papas"],
+    "Papas inglesas": [1.99, "Papas en rebanadas finas", "papasFritas.jpg", "Papas"],
+    "Papas rellenas": [2.99, "Papas en rellenas de queso", "papasRellenas.jpg", "Papas"]
+}
+
+Hamburguesas = {
+    "Tradicional": [3.99, "Hamburguesa tradicional con tocino y carne de res a la parrilla", "tradicional.jpg", "Hamburguesas"],
+    "Hamburguesa de pavo": [4.99, "Hamburguesa de pavo baja en grasas saturadas y calorías", "depavo.jpg", "Hamburguesas"],
+    "BBQ": [5.99, "Hamburguesa de carne de res con salsa BBQ dulce y ahumada encima", "bbq.jpg", "Hamburguesas"]
+}
+
+Bebidas = {
+    "Agua": [0.99, "Una botella de agua de 1 litro", "agua.jpg", "Bebidas"],
+    "Soda": [1.50, "Una lata de soda del sabor de tu preferencia", "sodas.jpg", "Bebidas"],
+    "Licuados": [2.50, "Licuado de guineo, fresa, papaya, guineo con fresa o de galleta", "licuados.jpg", "Bebidas"]
+}
+
+Entradas = {
+    "Alitas": [4.99, "Una orden de 10 alitas", "alitas.jpg", "Entradas"],
+    "Nachos": [4.99, "Una orden de nachos con chili, guacamole y queso", "nachos.jpg", "Entradas"],
+    "Pan con ajo": [3.99, "Una orden de 10 rebanadas de pan con ajo", "pan.jpeg", "Entradas"]
+}
+
+Postres = {
+    "Flan": [1.99, "Una porción de flan", "flan.jpg", "Postres"],
+    "Tartaleta": [1.25, "Una tartaleta de frutas", "tartaleta.jpg", "Postres"],
+    "Tres leches": [3.50, "Una porción de tres leches", "tresleches.jpg", "Postres"]
+}
+
+Guarniciones = {
+    "Arroz": [1.99, "Tazón de arroz para una persona", "arroz.jpg", "Guarniciones"],
+    "Pasta": [1.99, "Un plato de pasta", "pasta.jpg", "Guarniciones"],
+    "Ensalada": [1.99, "Un tazón de ensalada", "ensalada.jpg", "Guarniciones"]
+}
+
+Promociones = {
+    "Combo 1": [6.99, "1 hamburguesa tradicional, papas francesas y una soda", "combo1.jpg", "Promociones"],
+    "Combo 2": [7.99, "1 hamburguesa BBQ, papas inglesas y una soda", "combo2.jpg", "Promociones"],
+    "Combo para 2": [19.99, "2 hamburguesas tradicionales, 1 orden de alitas y 2 sodas", "comboduo.jpg", "Promociones"]
+}
+
+Promociones = {
+    "Combo Económico": [5.99, "Una hamburguesa tradicional con papas francesas", "comboeco.jpg", "Promociones"],
+    "Combo BBQ": [6.99, "Una hamburguesa BBQ con papas inglesas", "combobbq.jpg", "Promociones"],
+    "Combo Ligero": [7.49, "Una hamburguesa de pavo con ensalada", "comboligero.jpg", "Promociones"]
+}
+
+ParaCompartir = {
+    "Combo Amigos": [9.99, "2 hamburguesas tradicionales, 2 órdenes de papas francesas y 2 sodas", "comboamigos.jpg", "Para compartir"],
+    "Combo Pareja": [14.99, "1 hamburguesa BBQ, 1 hamburguesa de pavo, 1 orden de nachos y 2 sodas", "combopareja.jpg", "Para compartir"],
+    "Combo Familiar": [24.99, "3 hamburguesas tradicionales, 2 órdenes de alitas, 1 ensalada y 3 sodas", "combofamiliar.jpg", "Para compartir"]
+}
